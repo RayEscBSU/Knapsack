03692a0027a2240ebbb8dfd12b4b8289ec8625da
Raymundo <raymundoescobedo@u.boisestate.edu>
2022-12-02 11:26:19 -0700

    making it better?

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static BufferedReader br;
-    private static Scanner scan;
+    private static int[] wArray;
+    private static int[] vArray;
+    private static File wFile;
+    private static File vFile;
+    private static int lineCount;
+
     public static void main(String[] args) {
         int numItems = 0;
         int maxWeight = 0;
         String weight = null;
         String value = null;
         int debugLevel = 0;
-
-        int[] wArray;
-        int[] vArray;
 
 
         if (args.length > 3 && args.length < 6) {
@@ -36,46 +37,37 @@ public class Main {
             System.exit(1);
         }
         System.out.println("Num items: " + numItems + " Max weight: " + maxWeight + " Weight file: " + weight
-                + " Value file: " + value + " Debug option: " + debugLevel);
+                + " Value file: " + value + " Debug option: " + debugLevel + "\n");
 
         wArray = new int[numItems];
         vArray = new int[numItems];
         int lines;
 
-        File wFile= new File (weight);
-        File vFile = new File (value);
+        wFile = new File(weight);
+        vFile = new File(value);
 
         try {
-           lines= compareFiles(wFile,vFile);
+            compareFiles(wFile, vFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        BufferedReader br2;
-        try {
-           br = new BufferedReader(new FileReader(wFile));
-           br2 = new BufferedReader(new FileReader(vFile));
-           for (int i =0; i< lines; i++){
-               wArray[i]=Integer.parseInt(br.readLine());
-               vArray[i]=Integer.parseInt(br2.readLine());
-           }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        for (int i =0; i< lines; i++){
-            System.out.print( wArray[i] +" ");
+
+        //print out array
+        for (int i = 0; i < lineCount; i++) {
+            System.out.print(wArray[i] + " ");
 
         }
         System.out.println("");
-        for (int i =0; i< lines; i++){
-            System.out.print( vArray[i]+ " ");
+        for (int i = 0; i < lineCount; i++) {
+            System.out.print(vArray[i] + " ");
         }
+        System.out.println(" ");
 
 
-        System.out.println("done");
+        System.out.println("********done**********");
 
     }
+
 
     private static void Usage() {
         System.out.println("\n** Usage : java KnapsackBU <n> <W> <w.txt> <v.txt> [<debug level>] **");
@@ -85,11 +77,12 @@ public class Main {
         );
     }
 
-    private static int compareFiles(File x, File y) throws IOException {
+    private static void compareFiles(File x, File y) throws IOException {
+        BufferedReader br1;
+        BufferedReader br2;
         int countX = 0;
-        int countY= 0;
-        int count=0;
-        scan = new Scanner(x);
+        int countY = 0;
+        lineCount = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(x))) {
             String line;
@@ -103,14 +96,23 @@ public class Main {
                 countY++;
             }
         }
-        if (countX != countY){
+        if (countX != countY) {
             System.out.println("Files do not contain equal amounts of weight and value items");
-            System.out.println("File " + x + "lines: " + countX + "File " + y + "lines: " + countY );
+            System.out.println("File " + x + "lines: " + countX + "File " + y + "lines: " + countY);
             System.exit(1);
+        } else {
+            lineCount = countX;
+            if(wArray.length != lineCount){
+                System.out.println("The number of items entered  does not match number of items in files");
+                System.out.println("You enterd n: " + wArray.length + ", number of items in file: " + lineCount);
+                System.exit(1);
+            }
+            br1 = new BufferedReader(new FileReader(wFile));
+            br2 = new BufferedReader(new FileReader(vFile));
+            for (int i = 0; i < lineCount; i++) {
+                wArray[i] = Integer.parseInt(br1.readLine());
+                vArray[i] = Integer.parseInt(br2.readLine());
+            }
         }
-        else {
-            count = countX;
-        }
-        return count ;
     }
 }
